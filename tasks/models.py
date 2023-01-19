8a4a248546ed9bfea433ba7633a19b3f9e42e8cf
Ip.fabio <95320507+Ipfabio@users.noreply.github.com>
2023-01-18 21:03:22 -0300

    Update models.py
    Corrección en comentario

--- tasks/models.py
+++ tasks/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    created = models.DateTimeField(auto_now_add=True) # cuando creamo la tarea, añade la fecha y la hora por defecto en caso de que no le pasemos el dato
+    created = models.DateTimeField(auto_now_add=True) # cuando creamos la tarea, añade la fecha y la hora por defecto en caso de que no le pasemos el dato
     datecompleted = models.DateTimeField(null=True, blank=True)
     important = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE) # El modelo de usuario
